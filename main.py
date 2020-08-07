c92f2c9089b9e02c1e5deca92e6281749b0824a4
Pebaz <pebazium@gmail.com>
2020-08-06 21:16:15 -0400

    almost working

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-import toml, pprint
+import toml, pprint, math
+from raylib.dynamic import raylib as rl, ffi
+from raylib.colors import *
 
-pprint.pprint(toml.load(open('palettes/painting.toml')))
+TILE_SIZE = 64
+
+colors = toml.load(open('palettes/painting.toml'))
+pprint.pprint(colors)
+
+WIDTH = 640
+HEIGHT = 512
+
+rl.SetConfigFlags(rl.FLAG_WINDOW_RESIZABLE)
+rl.InitWindow(WIDTH, HEIGHT, b'Palettizer')
+rl.SetTargetFPS(30)
+
+while not rl.WindowShouldClose():
+    rl.BeginDrawing()
+    rl.ClearBackground(WHITE)
+
+    x, y = 0, 0
+    for name, color in colors.items():
+        r, g, b = color
+        rl.DrawRectangle(x, y, TILE_SIZE, TILE_SIZE, (r, g, b, 255))
+        x += TILE_SIZE
+        if x > WIDTH:
+            x = 0
+            y += 1
+    
+    rl.EndDrawing()
+
+    if rl.IsKeyPressed(rl.KEY_E):
+        rl.TakeScreenshot(b'pallet.png')
+
+rl.CloseWindow()
+
